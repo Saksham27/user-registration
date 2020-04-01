09a929686764aabd95812098fe85597495e216a9
Saksham27 <geekysaksham27@gmail.com>
2020-04-01 10:23:43 +0530

    Added: passes all email samples

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -3,10 +3,10 @@ using System.Text.RegularExpressions;
 
 namespace userRegistration
 {
-    class UserRegistration : Validator
+    class UserRegistration : Validator 
     {
 
-        public string checkMatch(string exp, string stringToMatch)
+        public string checkMatch(string exp, string stringToMatch) // interface method to match
         {
             var match = Regex.Match(stringToMatch, exp, RegexOptions.None);
             if (match.Success)
@@ -15,26 +15,26 @@ namespace userRegistration
                 return "invalid";
         }
 
-        public string validateName(string name)
+        public string validateName(string name) // interface method to validate name
         {
             var expr = @"^[A-Z]{1}[a-z]{2,}$";
             return this.checkMatch(expr, name);
             
         }
 
-        public string validateEmail(string email)
+        public string validateEmail(string email) // interface method to validate email
         {
             var expr = @"^abc((\.[A-Z]+[a-z]*[0-9]*)|(\.[A-Z]*[a-z]+[0-9]*)|(\.[A-Z]*[a-z]*[0-9]+)?)?@bl\.co(\.[a-z]{2,})?$";
             return this.checkMatch(expr, email);
         }
 
-        public string validateNumber(string number)
+        public string validateNumber(string number) // interface method to validate number
         {
             var expr = @"^([0-9]{2}[ ]{1}[0-9]{10})$";
             return this.checkMatch(expr, number);
         }
 
-        public string validatePassword(string password)
+        public string validatePassword(string password) // interface method to validate password
         {
             var expr = @"^(?=.*[A-Z])(?=.*[0-9])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)(.{8,})$";
             return this.checkMatch(expr, password);
@@ -55,7 +55,7 @@ namespace userRegistration
             string number = UserInput.inputNumber(); // taking number from user
             Console.WriteLine($"Number {userRes.validateNumber(number)}"); // validating number
 
-            string password = UserInput.inputPassword();
+            string password = UserInput.inputPassword(); // taking password from user
             Console.WriteLine($"Password {userRes.validatePassword(password)}"); // validating password
 
 
